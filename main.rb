93439008adc1300cb188fb53c09565757580b6c5
ecoologic <erikecoologic@gmail.com>
2016-05-21 22:21:43 +1000

    master card

--- main.rb
+++ main.rb
@@ -1,15 +1,10 @@
-  # CARD_TYPE_BY_REGEX = {
-  #   /^3[47][0-9]{13}$/           => :amex,
-  #   /^6011[0-9]{12}$/            => :discover,
-  #   /^5[1-5][0-9]{14}$/         => :mc,
-  #    => Visa
-  # }
 require 'ostruct'
 require 'pry'
 
 class CreditCard
   ALL = [
-    OpenStruct.new(name: "Visa", reg_exp: /^4[0-9]{12}(?:[0-9]{3})?$/)
+    OpenStruct.new(name: "Visa", reg_exp: /^4[0-9]{12}(?:[0-9]{3})?$/),
+    OpenStruct.new(name: "Master Card", reg_exp: /^5[1-5][0-9]{14}$/)
   ]
   UNKNOWN = OpenStruct.new(name: "Unknown")
 
@@ -29,24 +24,29 @@ class CreditCard
   attr_reader :cc_number
 
   def provider
-    ALL.detect { |p| cc_number =~ p[:reg_exp] } || UNKNOWN
+    ALL.detect { |p| cc_number =~ p.reg_exp } || UNKNOWN
   end
 end
 
-RSpec.configure do |config|
-  config.expect_with :rspec
-end
-
+RSpec.configure { |c| c.expect_with :rspec }
 RSpec.describe CreditCard do
   subject { described_class.new(cc_number) }
   let(:visa_number)        { 4111111111111111 }
+  let(:master_card_number) { 5105105105105106 }
   let(:implausible_number) { 411111111111111 }
 
-  context "a plausible visa number" do
+  context "a plausible Visa number" do
     let(:cc_number) { visa_number }
 
     it { expect(subject.plausible?).to eq(true)  }
     it { expect(subject.name).to eq('Visa') }
+  end
+
+  context "a plausible Master Card number" do
+    let(:cc_number) { master_card_number }
+
+    it { expect(subject.plausible?).to eq(true)  }
+    it { expect(subject.name).to eq('Master Card') }
   end
 
   context "an implausible number" do
